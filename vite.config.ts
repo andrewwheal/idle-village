71e59e247057a776e096e3878838962650f02370
Andrew Wheal <863704+andrewwheal@users.noreply.github.com>
2026-02-25 00:34:50 +0000

    chore: use terser to remove console.debug on prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: ["debug"],
+      }
+    },
+  }
 })
