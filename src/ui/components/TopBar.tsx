70edc38d9b776e5454ec8bff111b00da3355e1ab
Andrew Wheal <863704+andrewwheal@users.noreply.github.com>
2026-06-14 14:25:30 +0100

    debug: Add debug option to reset game while in development

--- src/ui/components/TopBar.tsx
+++ src/ui/components/TopBar.tsx
@@ -1,3 +1,6 @@
+import { RESOURCE_DEFINITIONS } from "../../content/resources";
+import { createInitialGameState } from "../../game/state";
+import { stateSignal } from "../../main";
 
 export default function TopBar() {
   console.log("Rendering TopBar");
@@ -8,6 +11,7 @@ export default function TopBar() {
           <div class="title">Idle Village</div>
           <div class="subtitle">Resources and buildings... other things to come</div>
         </div>
+        <button style={{ float: "right" }} type="button" onClick={() => { if (confirm("Are you sure you want to reset the game?")) { stateSignal.value = createInitialGameState(RESOURCE_DEFINITIONS); } }}>Reset Game</button>
       </div>
     </header>
   );
